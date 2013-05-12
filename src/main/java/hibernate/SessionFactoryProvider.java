47ecb8f15b63ecef757f0b82d246b3fffebbf880
Maciej <m.sandacz@o2.pl>
2013-05-12 17:37:57 +0200

    optymalizacja importu

--- src/main/java/hibernate/SessionFactoryProvider.java
+++ src/main/java/hibernate/SessionFactoryProvider.java
@@ -5,7 +5,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
-import server.DemoInitializer;
 import server.Initializer;
 
 public class SessionFactoryProvider {
