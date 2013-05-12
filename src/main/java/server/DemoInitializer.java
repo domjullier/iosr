393b2a1ba468393860cf41d390b9e741a55f7bd2
Dominique Jullier <domjullier@gmail.com>
2013-05-12 20:55:26 +0200

    Added roles for the new indexes to the users

--- src/main/java/server/DemoInitializer.java
+++ src/main/java/server/DemoInitializer.java
@@ -22,10 +22,10 @@ public class DemoInitializer implements Initializer {
 
         String[][] values = new String[][]{
                 {"PZU", "448.00", "PLN"},
-                {"PKO BP", "33.18", "PLN"},
-                {"KGHM POLSKA MIEDZ SA", "146.05", "PLN"},
+                {"PKO_BP", "33.18", "PLN"},
+                {"KGHM_POLSKA_MIEDZ_SA", "146.05", "PLN"},
                 {"PEKAO", "150.70", "PLN"},
-                {"PKN ORLEN", "48.32", "PLN"},
+                {"PKN_ORLEN", "48.32", "PLN"},
                 {"PGE", "16.81", "PLN"},
                 {"PGNIG", "5.68", "PLN"},
                 {"LPP", "7011.00", "PLN"},
