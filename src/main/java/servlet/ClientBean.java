bc0a4f554cae79105d85106d4cf0356230ef8ac0
Dominique Jullier <domjullier@gmail.com>
2013-05-25 19:56:11 +0200

    Commit from JBoss Tools

--- src/main/java/servlet/ClientBean.java
+++ src/main/java/servlet/ClientBean.java
@@ -6,6 +6,7 @@ import model.Index;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,6 +15,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+@WebServlet("/Client")
 public class ClientBean extends HttpServlet {
 
 	private static final long serialVersionUID = -6948827418149135145L;
