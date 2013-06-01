e330f3b155ecbd34d3e50589c6ac03c2c3fda5c4
Dominique Jullier <domjullier@gmail.com>
2013-06-01 19:48:34 +0200

    Commit from JBoss Tools

--- src/main/java/servlet/ClientSend.java
+++ src/main/java/servlet/ClientSend.java
@@ -1,19 +1,4 @@
-/*
- * JBoss, Home of Professional Open Source
- * Copyright 2012, Red Hat, Inc. and/or its affiliates, and individual
- * contributors by the @authors tag. See the copyright.txt in the 
- * distribution for a full listing of individual contributors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,  
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package servlet;
 
 import java.io.IOException;
@@ -67,11 +52,11 @@ public class ClientSend extends HttpServlet {
 		
 		try {
 		    Destination destination;
-		    if (req.getParameterMap().keySet().contains("topic")) {
-		        destination = topic;
-		    } else {
+		    //if (req.getParameterMap().keySet().contains("topic")) {
+		    //    destination = topic;
+		   // } else {
 		        destination = queue;
-		    }
+		    //}
 		    out.write("<H2>Sending messages to <em>" + destination + "</em></H2>");
 			connection = connectionFactory.createConnection();
 			Session session = connection.createSession(false,
--- src/test/java/server/ServerTest.java
+++ src/test/java/server/ServerTest.java
@@ -1,8 +1,17 @@
 package server;
 
+import static org.testng.AssertJUnit.assertEquals;
+
+import java.net.URL;
+
 import org.junit.*;
 import org.testng.annotations.Test;
 
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+
+import net.sourceforge.jwebunit.util.TestContext;
 import static net.sourceforge.jwebunit.junit.JWebUnit.*;
 
 public class ServerTest {
@@ -13,9 +22,11 @@ public class ServerTest {
     }
 
     @Test
-    public void testLogin() {
+    public void testLogin() throws Exception {
+    	
+    	
     	setBaseUrl("http://market-jullier.rhcloud.com");
-        beginAt("/index.html");
+    	beginAt("/index.html");
         //clickLink("login");
         assertTitleEquals("Login Form");
         setTextField("j_username", "user1");
@@ -25,12 +36,22 @@ public class ServerTest {
         assertTitleEquals("Broker Client - Main menu");
         
         //change value and check it on the server
-        setTextField("Value", "667");
+        setWorkingForm(1);
+        setTextField("Value", "167");
+        setHiddenField("index", "PZU");
         submit();
+        assertMatch("Sent update for index:");
         
         //Check on server, if the value got updated
         gotoPage("http://market-jullier.rhcloud.com/Server");
-        assertMatch("^PZU 667.00 PLN");
+        assertMatch("^PZU 167.00 PLN");
         
+        //reset to default value
+        gotoPage("http://market-jullier.rhcloud.com/Client");
+        setWorkingForm(1);
+        setTextField("Value", "12");
+        setHiddenField("index", "PZU");
+        submit();
+        assertMatch("Sent update for index:");
     }
 }
